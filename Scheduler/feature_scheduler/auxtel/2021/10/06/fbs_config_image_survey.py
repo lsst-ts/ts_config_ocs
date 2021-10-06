5183e5d7d254ec15753c3dc1062c714e10394fa4
Tiago Ribeiro <tribeiro@lsst.org>
2021-10-06 11:29:42 -0700

    Add configuration for image survey. Minor fixes to spectroscopic survey.

--- Scheduler/feature_scheduler/auxtel/2021/10/06/fbs_config_image_survey.py
+++ Scheduler/feature_scheduler/auxtel/2021/10/06/fbs_config_image_survey.py
@@ -4,9 +4,10 @@ import numpy as np
 
 import astropy.units as u
 
-import matplotlib.pyplot as plt
+from pathlib import Path
 
 from astropy.coordinates import Angle
+from astropy.io import ascii as astropy_ascii
 
 from rubin_sim.scheduler import basis_functions
 
@@ -15,7 +16,7 @@ from rubin_sim.scheduler.schedulers import Core_scheduler
 from rubin_sim.scheduler.surveys import Greedy_survey, FieldSurvey
 
 
-def get_basis_functions_sp_survey(ra, nside, note, ha_limits, frac_total):
+def get_basis_functions_image_survey(ra, nside, note, ha_limits):
     sun_alt_limit = -18.0
     time_needed = 62.0
 
@@ -24,76 +25,73 @@ def get_basis_functions_sp_survey(ra, nside, note, ha_limits, frac_total):
         basis_functions.Time_to_twilight_basis_function(time_needed=time_needed),
         basis_functions.Hour_Angle_limit_basis_function(RA=ra, ha_limits=ha_limits),
         basis_functions.M5_diff_basis_function(nside=nside),
-        basis_functions.Slewtime_basis_function(nside=nside),
+        basis_functions.Slewtime_basis_function(nside=nside, filtername="g"),
         basis_functions.Moon_avoidance_basis_function(nside=nside),
         basis_functions.Zenith_shadow_mask_basis_function(
-            min_alt=28.0, max_alt=85.5, nside=nside
+            min_alt=28.0, max_alt=85.0, nside=nside
         ),
-        basis_functions.VisitGap(note=note, gap_min=15.0),
+        basis_functions.VisitGap(note=note, gap_min=1440.0),
     ]
 
 
 def get_basis_functions_cwfs_survey(nside, note, time_gap_min):
     sun_alt_limit = -12.0
+    weights = [1.0, 1000.0, 1.0, 1.0, 1]
     return [
         basis_functions.Not_twilight_basis_function(sun_alt_limit=sun_alt_limit),
-        basis_functions.Slewtime_basis_function(nside=nside),
+        basis_functions.Slewtime_basis_function(nside=nside, filtername="g"),
         basis_functions.Moon_avoidance_basis_function(nside=nside),
         basis_functions.Zenith_shadow_mask_basis_function(
-            min_alt=28.0, max_alt=85.5, nside=nside
+            min_alt=28.0, max_alt=85.0, nside=nside
         ),
         basis_functions.VisitGap(note=note, gap_min=time_gap_min),
-    ]
+    ], weights
 
 
 if __name__ == "config":
 
     nside = 32
 
+    path = Path(__file__).parent
+    tiles = astropy_ascii.read(path / "latiss_tiles_goods.txt")
+
     reward_value = 10
     nexp = 1
-    ha_limit = [[18.0, 24.0], [0.0, 6.0]]
+    ha_limit = [[24.0 - 2.5, 24.0], [0.0, 2.5]]
 
     target_list = [
-        ("HD2811", "00:31:18", "-43:36:23.0", ha_limit, reward_value),
-        ("HD009051", "01:28:46", "-24:20:25.4", ha_limit, reward_value),
-        ("HD14943", "02:22:54", "-51:05:31.7", ha_limit, reward_value),
-        ("HD031128", "04:52:09", "-27:03:50.9", ha_limit, reward_value),
-        ("LAMLEP", "05:19:34", "-13:10:36.4", ha_limit, reward_value),
-        ("HD37962", "05:40:51", "-31:21:04.0", ha_limit, reward_value),
-        ("MUCOL", "05:46:00", "-32:18:23.2", ha_limit, reward_value),
-        ("HD38949", "05:48:20", "-24:27:49.9", ha_limit, reward_value),
-        ("ETA1DOR", "06:06:09", "-66:02:23", ha_limit, reward_value),
-        ("HD185975", "20:28:18", "-87:28:19.9", [[0.0, 24.0]], reward_value * 2.0),
-        ("HD200654", "21:06:34", "-49:57:50.3", ha_limit, reward_value),
-        ("HD205905", "21:39:10", "-27:18:23.7", ha_limit, reward_value),
+        (tile["Name"], tile["RA"], tile["Dec"], ha_limit, reward_value)
+        for tile in tiles
     ]
 
     surveys = [[], []]
 
-    bfs = get_basis_functions_cwfs_survey(nside=32, note="cwfs", time_gap_min=120.0)
+    bfs, weights = get_basis_functions_cwfs_survey(
+        nside=nside, note="cwfs", time_gap_min=120.0
+    )
 
     surveys[0].append(
         Greedy_survey(
-            bfs, np.ones_like(bfs) * 10.0, nside=32, survey_name="cwfs", nexp=4
+            bfs, weights, nside=nside, survey_name="cwfs", nexp=4, filtername="g"
         )
     )
 
     for name, ra_str, dec_str, ha_limits, reward in target_list:
+
         ra = Angle(ra_str, unit=u.hourangle).to(u.deg).value
         dec = Angle(dec_str, unit=u.deg).to(u.deg).value
 
-        bfs = get_basis_functions_sp_survey(
-            ra=ra, nside=32, note=name, ha_limits=ha_limits, frac_total=0.2
+        bfs = get_basis_functions_image_survey(
+            ra=ra, nside=nside, note=name, ha_limits=ha_limits
         )
 
         observation = empty_observation()
         observation["RA"] = np.radians(ra)
         observation["dec"] = np.radians(dec)
-        observation["filter"] = "r"
-        observation["exptime"] = 130.0
-        observation["nexp"] = 1.0
-        observation["note"] = f"spec:{name}"
+        observation["filter"] = "g"
+        observation["exptime"] = 30.0
+        observation["nexp"] = 2
+        observation["note"] = f"{name}"
         sequence = [observation]
 
         surveys[1].append(
