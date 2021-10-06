554a76dac8e16d23f1e9e105b5e5a8bd91ef3397
Tiago Ribeiro <tribeiro@lsst.org>
2021-10-05 21:34:18 -0700

    Update feature scheduler configuration.

--- Scheduler/feature_scheduler/auxtel/2021/10/07/fbs_config_spec_survey.py
+++ Scheduler/feature_scheduler/auxtel/2021/10/07/fbs_config_spec_survey.py
@@ -9,119 +9,10 @@ import matplotlib.pyplot as plt
 from astropy.coordinates import Angle
 
 from rubin_sim.scheduler import basis_functions
-from rubin_sim.scheduler import features
-from rubin_sim.scheduler import sim_runner
 
 from rubin_sim.scheduler.utils import empty_observation
-from rubin_sim.utils import _raDec2Hpid
-from rubin_sim.scheduler.schedulers import Core_scheduler, simple_filter_sched
-from rubin_sim.scheduler.surveys import Greedy_survey, Deep_drilling_survey
-from rubin_sim.scheduler.modelObservatory import Model_observatory
-
-
-class NoteLastObserved(features.BaseSurveyFeature):
-    """Track when the last observation that matches a certain condition
-    was made.
-    """
-
-    def __init__(self, note):
-        self.note = note
-        self.feature = None
-
-    def add_observation(self, observation, indx=None):
-        if self.note in observation["note"]:
-            self.feature = observation["mjd"]
-
-
-class VisitGap(basis_functions.Base_basis_function):
-    def __init__(self, note, gap_min=25.0, penalty_val=np.nan):
-        super().__init__()
-        self.penalty_val = penalty_val
-
-        self.gap = gap_min / 60.0 / 24.0
-        self.survey_features = dict()
-        self.survey_features["NoteLastObserved"] = NoteLastObserved(note=note)
-
-    def _check_feasibility(self, conditions):
-        if self.survey_features["NoteLastObserved"].feature is None:
-            return True
-
-        diff = conditions.mjd - self.survey_features["NoteLastObserved"].feature
-
-        if diff <= self.gap:
-            return False
-        else:
-            return True
-
-    def check_feasibility(self, conditions):
-        return self._check_feasibility(conditions)
-
-    def _calc_value(self, conditions, indx=None):
-        return 1.0 if self._check_feasibility(conditions) else self.penalty_val
-
-
-class FieldSurvey(Deep_drilling_survey):
-    def __init__(
-        self,
-        basis_functions,
-        RA,
-        dec,
-        sequence="rgizy",
-        nvis=[20, 10, 20, 26, 20],
-        exptime=30.0,
-        u_exptime=30.0,
-        nexp=2,
-        ignore_obs=None,
-        survey_name="DD",
-        reward_value=None,
-        readtime=2.0,
-        filter_change_time=120.0,
-        nside=None,
-        flush_pad=30.0,
-        seed=42,
-        detailers=None,
-    ):
-        super().__init__(
-            basis_functions=basis_functions,
-            RA=RA,
-            dec=dec,
-            sequence=sequence,
-            nvis=nvis,
-            exptime=exptime,
-            u_exptime=u_exptime,
-            nexp=nexp,
-            ignore_obs=ignore_obs,
-            survey_name=survey_name,
-            reward_value=reward_value,
-            readtime=readtime,
-            filter_change_time=filter_change_time,
-            nside=nside,
-            flush_pad=flush_pad,
-            seed=seed,
-            detailers=detailers,
-        )
-        self.basis_weights = np.ones(len(basis_functions)) / len(basis_functions)
-
-    def calc_reward_function(self, conditions):
-        self.reward_checked = True
-        indx = _raDec2Hpid(self.nside, self.ra, self.dec)
-        if self._check_feasibility(conditions):
-            self.reward = 0
-            indx = _raDec2Hpid(self.nside, self.ra, self.dec)
-            for bf, weight in zip(self.basis_functions, self.basis_weights):
-                basis_value = bf(conditions, indx=indx)
-                self.reward += basis_value * weight
-
-            self.reward = np.sum(self.reward[indx])
-
-            if np.any(np.isinf(self.reward)):
-                self.reward = np.inf
-        else:
-            # If not feasable, negative infinity reward
-            self.reward = -np.inf
-            return self.reward
-
-        return self.reward
+from rubin_sim.scheduler.schedulers import Core_scheduler
+from rubin_sim.scheduler.surveys import Greedy_survey, FieldSurvey
 
 
 def get_basis_functions_sp_survey(ra, nside, note, ha_limits, frac_total):
@@ -138,7 +29,7 @@ def get_basis_functions_sp_survey(ra, nside, note, ha_limits, frac_total):
         basis_functions.Zenith_shadow_mask_basis_function(
             min_alt=28.0, max_alt=85.5, nside=nside
         ),
-        VisitGap(note=note, gap_min=15.0),
+        basis_functions.VisitGap(note=note, gap_min=15.0),
     ]
 
 
@@ -151,7 +42,7 @@ def get_basis_functions_cwfs_survey(nside, note, time_gap_min):
         basis_functions.Zenith_shadow_mask_basis_function(
             min_alt=28.0, max_alt=85.5, nside=nside
         ),
-        VisitGap(note=note, gap_min=time_gap_min),
+        basis_functions.VisitGap(note=note, gap_min=time_gap_min),
     ]
 
 
