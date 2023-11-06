7066ceaf74abdc496e87cdef74340b44ed06fb8f
edennihy <edennihy@lsst.org>
2023-11-06 10:51:13 -0700

    ATScheduler: update auxtel-photocalib-imaging block to include images with SDSSi_65mm in grating wheel and update surveys to match.

--- Scheduler/feature_scheduler/auxtel/fbs_config_image_photocal_survey.py
+++ Scheduler/feature_scheduler/auxtel/fbs_config_image_photocal_survey.py
@@ -118,7 +118,7 @@ def get_scheduler():
             survey_name="AUXTEL_PHOTO_IMAGING",
             hour_angle_limit=image_ha_limit,
             reward_value=reward_values["default"],
-            filters=["g", "r", "i"],
+            filters=["g"],
             visit_gap=image_visit_gap,
             exptime=image_exptime,
             nexp=image_nexp,
