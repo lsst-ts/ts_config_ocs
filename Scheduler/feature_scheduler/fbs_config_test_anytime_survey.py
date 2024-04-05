eab18405b4fcb98d0b3b0d7dd31b1d1042038281
Tiago Ribeiro <tribeiro@lsst.org>
2024-04-05 11:47:27 -0700

    Scheduler: Upfate filter set for test anytime survey configuration.

--- Scheduler/feature_scheduler/fbs_config_test_anytime_survey.py
+++ Scheduler/feature_scheduler/fbs_config_test_anytime_survey.py
@@ -33,7 +33,7 @@ def gen_greedy_surveys(
     nside=32,
     nexp=1,
     exptime=30.0,
-    filters=["r", "i", "z", "y"],
+    filters=["g", "r", "z"],
     camera_rot_limits=[-80.0, 80.0],
     shadow_minutes=60.0,
     max_alt=76.0,
