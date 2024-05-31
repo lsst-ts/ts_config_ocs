38bca4bcce745fba953d407118813f427a77878b
Keith Bechtol <kbechtol@lsst.org>
2024-05-31 06:20:46 +0000

    Add minimal working example science program field survey

---
+++ Scheduler/feature_scheduler/maintel/fbs_config_fieldsurvey.py
@@ -0,0 +1,47 @@
+# This file is part of ts_config_ocs.
+#
+# Developed for the Vera Rubin Observatory Telescope and Site System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from lsst.ts.fbs.utils.maintel.make_fieldsurvey_scheduler import MakeFieldSurveyScheduler
+
+
+def get_scheduler():
+    """Construct feature based scheduler.
+
+    Returns
+    -------
+    nside : int
+        Healpix map resolution.
+    scheduler : Core_scheduler
+        Feature based scheduler.
+    """
+
+    make_scheduler = MakeFieldSurveyScheduler()
+
+    field_names = [
+        "EDFS_A", 
+        "EDFS_B",
+    ]
+    make_scheduler.add_field_surveys(field_names)
+    
+    return make_scheduler.get_scheduler()
+
+if __name__ == "config":
+    nside, scheduler = get_scheduler()
