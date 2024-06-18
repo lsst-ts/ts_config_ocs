f4a1a0018de39c20efae74f4edcbe94ab2ba34f3
Wouter van Reeven <wvanreeven@lsst.org>
2024-06-18 13:44:13 +0200

    ESS: Add test to validate the DataClient configurations.

---
+++ tests/test_ESS_DataClient_config.py
@@ -0,0 +1,86 @@
+# This file is part of ts_config_ocs.
+#
+# Developed for Vera C. Rubin Observatory Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import pathlib
+import unittest
+
+from lsst.ts import salobj
+from lsst.ts.salobj.configurable_csc import ConfigurableCsc
+from lsst.ts.salobj.validator import StandardValidator
+
+try:
+    from lsst.ts.ess import common, csc
+except ImportError:
+    raise unittest.SkipTest("Cannot import ESS modules.")
+
+
+class ConfigTestCase(salobj.BaseConfigTestCase, unittest.TestCase):
+    def setUp(self):
+        self.config_package_root = pathlib.Path(__file__).parents[1]
+
+    def test_ESS_DataClient_config(self):
+        """Validate the ESS DataClient configuration.
+
+        The ESS CSC itself only defines the general configuration schema. That
+        schema indicates that there is more than one CSC instance and that each
+        instance can define one or more DataClients. The configuration of each
+        DataClient is defined in DataClient-specific schemas inside the
+        DataClient classes.
+
+        A separate test is required because the generic configuration file
+        tests in test_config_files.py only validate the CSC schema and not any
+        sub-schemas if they exist.
+        """
+        config_package_root = self.config_package_root
+        sal_name = "ESS"
+
+        # Validate that there is only one config file.
+        config_dir_name = self.get_config_dir(
+            config_package_root=config_package_root,
+            sal_name=sal_name,
+            schema=csc.CONFIG_SCHEMA,
+        )
+        config_dir = pathlib.Path(config_dir_name)
+        config_files = list(config_dir.glob("*.yaml"))
+        assert len(config_files) == 1, "Expected only one ESS configuration file."
+
+        # Validate that the expected config file exists.
+        config_file = config_dir / "_init.yaml"
+        assert config_file.exists(), "Didn't find ESS config file _init.yaml."
+
+        # Load the config.
+        config = ConfigurableCsc.read_config_files(
+            config_validator=StandardValidator(csc.CONFIG_SCHEMA),
+            config_dir=config_dir,
+            files_to_read=["_init.yaml"],
+        )
+
+        # Loop over the CSC instances and validate the config for all the
+        # DataClients of each instance.
+        for instance in config.instances:
+            for client_data in instance["data_clients"]:
+                client_class = common.data_client.get_data_client_class(
+                    client_data["client_class"]
+                )
+                config_schema = client_class.get_config_schema()
+                validator = salobj.DefaultingValidator(config_schema)
+                client_config_dict = client_data["config"]
+                validator.validate(client_config_dict)
