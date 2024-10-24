d7c48e81d426e3ce081d71a7175f0c9abc2c0c03
David Sanmartim <davidsanm@gmail.com>
2024-10-23 23:45:38 -0300

    MTScheduler: updated scheduler config for BLOCK-T193

--- Scheduler/feature_scheduler/maintel/fbs_config_sit_survey_block_t193.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_sit_survey_block_t193.py
@@ -94,9 +94,15 @@ def gen_greedy_surveys(
     }
 
     surveys = []
-    detailer = detailers.CameraRotDetailer(
-        min_rot=np.min(camera_rot_limits), max_rot=np.max(camera_rot_limits)
-    )
+    survey_detailers = [
+        detailers.TrackingInfoDetailer(
+            science_program=greed_survey_params["survey_name"]
+        ),
+        detailers.CameraRotDetailer(
+            min_rot=np.min(camera_rot_limits), max_rot=np.max(camera_rot_limits)
+        ),
+    ]
+
 
     for filtername in filters:
         bfs = [
@@ -137,7 +143,7 @@ def gen_greedy_surveys(
                 nside=nside,
                 ignore_obs=ignore_obs,
                 nexp=nexp,
-                detailers=[detailer],
+                detailers=survey_detailers,
                 **greed_survey_params,
             )
         )
