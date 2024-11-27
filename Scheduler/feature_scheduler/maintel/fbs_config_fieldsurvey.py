1122d70c11fcf8633ee63ea5864ca376ac8ef657
Lynne Jones <ljones.uw@gmail.com>
2024-11-26 22:27:20 -0800

    First modification; needs testing

--- Scheduler/feature_scheduler/maintel/fbs_config_fieldsurvey.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_fieldsurvey.py
@@ -19,6 +19,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import numpy as np
+from rubin_scheduler.scheduler.utils import ObservationArray
+from rubin_scheduler.scheduler.detailers import StartFieldSequenceDetailer
+
 from lsst.ts.fbs.utils.maintel.basis_functions import get_basis_functions_field_survey
 from lsst.ts.fbs.utils.maintel.detailers import get_detailers_field_survey
 from lsst.ts.fbs.utils.maintel.make_fieldsurvey_scheduler import (
@@ -40,17 +44,39 @@ def get_scheduler():
 
     nside = 32
 
+    # Scheduler will be the field survey scheduler
     make_scheduler = MakeFieldSurveyScheduler(nside=nside, ntiers=1)
 
+    target_info = get_comcam_sv_targets()
+    # Edit HERE to reduce the number of fields included each night
+    target_names = target_info.keys()
+
+    # Main survey metadata
+    observation_reason = "science"
+    science_program = "BLOCK-320"  # json BLOCK to be used
+
+    # And AOS visits will be configured and run as a detailer on top of
+    # each field survey.
+    aos_scheduler_note = "Close Loop sequences"
+    aos_science_program = "BLOCK-330"
+    # For each AOS detailer (which must be separately defined)
+    # we will add an ObservationArray specifically for that target.
+    aos_obs_lists = {}
+    for target in target_names:
+        obslist = ObservationArray()
+        obslist["exptime"] = 90
+        obslist["RA"] = np.radians(target_info[target]["ra"])
+        obslist["dec"] = np.radians(target_info[target]["dec"])
+        obslist["rotTelPos"] = np.radians(0)
+        aos_obs_lists[target] = [obslist]
+
+    # Define the standard sequence for all field surveys.
     nvisits = {"u_02": 20, "g_01": 20, "r_03": 20, "i_06": 20, "z_03": 20, "y": 20}
     sequence = ["u_02", "g_01", "r_03", "i_06", "z_03", "y"]
     # exposure time in seconds
     exptimes = {"u_02": 38, "g_01": 30, "r_03": 30, "i_06": 30, "z_03": 30, "y": 30}
-    # 1 --> single 30 second exposuree
+    # 1 --> single 30 second exposure
     nexps = {"u_02": 1, "g_01": 1, "r_03": 1, "i_06": 1, "z_03": 1, "y": 1}
-
-    wind_speed_maximum = 20.0  # maximum direct wind in m/s
-    min_alt = 40.0
 
     field_survey_kwargs = {
         "nvisits": nvisits,
@@ -59,30 +85,40 @@ def get_scheduler():
         "nexps": nexps,
     }
 
+    # Define basis functions to choose between field surveys
+    wind_speed_maximum = 20.0  # maximum direct wind in m/s
+    min_alt = 40.0
+
     basis_functions = get_basis_functions_field_survey(
         nside=nside,
         wind_speed_maximum=wind_speed_maximum,
         min_alt=min_alt,
     )
 
+    # Generic detailers
     detailers = get_detailers_field_survey()
 
-    observation_reason = "science"
-    science_program = "BLOCK-320"  # json BLOCK to be used
-    survey_name = science_program  # match nextVisit metadata
-
     tier = 0
-    target_names = get_comcam_sv_targets().keys()
-    make_scheduler.add_field_surveys(
-        tier,
-        observation_reason,
-        science_program,
-        target_names,
-        survey_name=survey_name,
-        basis_functions=basis_functions,
-        detailers=detailers,
-        **field_survey_kwargs,
-    )
+    for target in target_names:
+        aos_detailer = StartFieldSequenceDetailer(
+            sequence_obs_list=aos_obs_lists[target],
+            ang_distance_match=3.5,
+            time_match_hours=2,
+            ra=target_info[target]["ra"],
+            dec=target_info[target]["dec"],
+            scheduler_note=aos_scheduler_note,
+            science_program=aos_science_program,
+        )
+        make_scheduler.add_field_surveys(
+            tier,
+            observation_reason,
+            science_program,
+            [target],
+            survey_name=target,
+            basis_functions=basis_functions,
+            detailers=[aos_detailer] + detailers,
+            **field_survey_kwargs,
+        )
 
     return make_scheduler.get_scheduler()
 
--- Scheduler/feature_scheduler/maintel/fbs_config_minimal_fieldsurvey.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_minimal_fieldsurvey.py
@@ -19,11 +19,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import numpy as np
 from lsst.ts.fbs.utils.maintel.make_fieldsurvey_scheduler import (
     MakeFieldSurveyScheduler,
     get_comcam_sv_targets,
 )
 from rubin_scheduler.scheduler import basis_functions, detailers
+from rubin_scheduler.scheduler.utils import ObservationArray
 
 
 def get_scheduler():
@@ -41,8 +43,91 @@ def get_scheduler():
 
     make_scheduler = MakeFieldSurveyScheduler(nside=nside, ntiers=1)
 
+    # Main science program setup
+    observation_reason = "science"
+    science_program = "BLOCK-320"  # json BLOCK to be used
+
+    # AOS visits will be configured and run as a detailer on top of
+    # each field survey.
+    aos_scheduler_note = "Close Loop sequences"
+    aos_science_program = "BLOCK-330"
+    # For each AOS detailer (which must be separately defined)
+    # we will add an ObservationArray specifically for that target.
+    # (it has to know about the RA/Dec of the target field).
+    target_info = get_comcam_sv_targets()
+    aos_obs_lists = {}
+    for target in target_info:
+        obslist = ObservationArray()
+        obslist["exptime"] = 90
+        obslist["RA"] = np.radians(target_info[target]["ra"])
+        obslist["dec"] = np.radians(target_info[target]["dec"])
+        obslist["rotTelPos"] = np.radians(0)
+        # I'm assuming that most of the aos_obs request will be overridden by
+        # the json block, but the scheduler_note should be maintained
+        # and the result of this observation should be fed back to the FBS
+        # (like any other requested observation).
+        aos_obs_lists[target] = [obslist]
+
+    # Now configure main science surveys
+    # All surveys have the same basis functions
+    config_basis_functions = [
+        basis_functions.NotTwilightBasisFunction(sun_alt_limit=-12.0),
+        basis_functions.AltAzShadowMaskBasisFunction(
+            nside=nside,
+            min_alt=40.0,
+            max_alt=83.0,
+            shadow_minutes=2.0,
+        ),
+        basis_functions.SlewtimeBasisFunction(filtername="u_02", nside=nside),
+        basis_functions.SlewtimeBasisFunction(filtername="g_01", nside=nside),
+        basis_functions.SlewtimeBasisFunction(filtername="r_03", nside=nside),
+        basis_functions.SlewtimeBasisFunction(filtername="i_06", nside=nside),
+        basis_functions.SlewtimeBasisFunction(filtername="z_03", nside=nside),
+        basis_functions.SlewtimeBasisFunction(filtername="y_04", nside=nside),
+    ]
+
+    # Configure ecliptic target survey
     ecliptic_targets = ["Rubin_SV_38_7"]
 
+    nvisits = {"u_02": 8, "g_01": 16, "r_03": 8, "i_06": 8, "z_03": 8, "y_04": 8}
+    sequence = ["g_01", "r_03", "i_06"]
+    # exposure time in seconds
+    exptimes = {"u_02": 38, "g_01": 30, "r_03": 30, "i_06": 30, "z_03": 30, "y_04": 30}
+    # 1 --> single 30 second exposure
+    nexps = {"u_02": 1, "g_01": 1, "r_03": 1, "i_06": 1, "z_03": 1, "y_04": 1}
+
+    field_survey_kwargs = {
+        "nvisits": nvisits,
+        "sequence": sequence,
+        "exptimes": exptimes,
+        "nexps": nexps,
+    }
+
+    # Detailers for all ecliptic surveys
+    config_detailers = [detailers.ComCamGridDitherDetailer()]
+
+    tier = 0
+    for target in ecliptic_targets:
+        aos_detailer = detailers.StartFieldSequenceDetailer(
+            sequence_obs_list=aos_obs_lists[target],
+            ang_distance_match=3.5,
+            time_match_hours=2,
+            ra=target_info[target]["ra"],
+            dec=target_info[target]["dec"],
+            scheduler_note=aos_scheduler_note,
+            science_program=aos_science_program,
+        )
+        make_scheduler.add_field_surveys(
+            tier,
+            observation_reason,
+            science_program,
+            [target],
+            basis_functions=config_basis_functions,
+            detailers=[aos_detailer] + config_detailers,
+            **field_survey_kwargs,
+        )
+
+    # And now configure standard survey fields
     # ComCam Deep Drilling Fields
 
     nvisits = {"u_02": 5, "g_01": 10, "r_03": 5, "i_06": 5, "z_03": 5, "y_04": 5}
@@ -59,23 +144,6 @@ def get_scheduler():
         "nexps": nexps,
     }
 
-    config_basis_functions = [
-        basis_functions.NotTwilightBasisFunction(sun_alt_limit=-12.0),
-        basis_functions.AltAzShadowMaskBasisFunction(
-            nside=nside,
-            min_alt=40.0,
-            max_alt=83.0,
-            shadow_minutes=2.0,
-        ),
-        basis_functions.SlewtimeBasisFunction(filtername="u_02", nside=nside),
-        basis_functions.SlewtimeBasisFunction(filtername="g_01", nside=nside),
-        basis_functions.SlewtimeBasisFunction(filtername="r_03", nside=nside),
-        basis_functions.SlewtimeBasisFunction(filtername="i_06", nside=nside),
-        basis_functions.SlewtimeBasisFunction(filtername="z_03", nside=nside),
-        basis_functions.SlewtimeBasisFunction(filtername="y_04", nside=nside),
-        basis_functions.FilterLoadedBasisFunction(filternames=sequence),
-    ]
-
     config_detailers = [
         detailers.DitherDetailer(max_dither=0.2, per_night=False),
         detailers.CameraSmallRotPerObservationListDetailer(
@@ -83,53 +151,30 @@ def get_scheduler():
         ),
     ]
 
-    observation_reason = "science"
-    science_program = "BLOCK-320"  # json BLOCK to be used
-
     tier = 0
     target_names = get_comcam_sv_targets(
         exclude=ecliptic_targets,
     ).keys()
-    make_scheduler.add_field_surveys(
-        tier,
-        observation_reason,
-        science_program,
-        target_names,
-        basis_functions=config_basis_functions,
-        detailers=config_detailers,
-        **field_survey_kwargs,
-    )
+    for target in target_names:
+        aos_detailer = detailers.StartFieldSequenceDetailer(
+            sequence_obs_list=aos_obs_lists[target],
+            ang_distance_match=3.5,
+            time_match_hours=2,
+            ra=target_info[target]["ra"],
+            dec=target_info[target]["dec"],
+            scheduler_note=aos_scheduler_note,
+        )
+        make_scheduler.add_field_surveys(
+            tier,
+            observation_reason,
+            science_program,
+            [target],
+            basis_functions=config_basis_functions,
+            detailers=[aos_detailer] + config_detailers,
+            **field_survey_kwargs,
+        )
 
     # Ecliptic Field
-
-    nvisits = {"u_02": 8, "g_01": 16, "r_03": 8, "i_06": 8, "z_03": 8, "y_04": 8}
-    sequence = ["g_01", "r_03", "i_06"]
-    # exposure time in seconds
-    exptimes = {"u_02": 38, "g_01": 30, "r_03": 30, "i_06": 30, "z_03": 30, "y_04": 30}
-    # 1 --> single 30 second exposure
-    nexps = {"u_02": 1, "g_01": 1, "r_03": 1, "i_06": 1, "z_03": 1, "y_04": 1}
-
-    field_survey_kwargs = {
-        "nvisits": nvisits,
-        "sequence": sequence,
-        "exptimes": exptimes,
-        "nexps": nexps,
-    }
-
-    config_detailers = [detailers.ComCamGridDitherDetailer()]
-    config_detailers[0].survey_features = {}
-
-    tier = 0
-    target_names = ecliptic_targets
-    make_scheduler.add_field_surveys(
-        tier,
-        observation_reason,
-        science_program,
-        target_names,
-        basis_functions=config_basis_functions,
-        detailers=config_detailers,
-        **field_survey_kwargs,
-    )
 
     return make_scheduler.get_scheduler()
 
