d6958c6a5c585f2e1d526fdf1c5ff891212041c8
Keith Bechtol <kbechtol@lsst.org>
2024-12-06 23:25:13 +0000

    MTScheduler: use gri sequence for FBS SV survey

--- Scheduler/feature_scheduler/maintel/fbs_config_minimal_fieldsurvey.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_minimal_fieldsurvey.py
@@ -45,8 +45,8 @@ def get_scheduler():
 
     # ComCam Deep Drilling Fields
 
-    nvisits = {"u_02": 5, "g_01": 5, "r_03": 5, "i_06": 5, "z_03": 5, "y_04": 5}
-    sequence = ["g_01"]
+    nvisits = {"u_02": 5, "g_01": 10, "r_03": 5, "i_06": 5, "z_03": 5, "y_04": 5}
+    sequence = ["g_01", "r_03", "i_06"]
     # exposure time in seconds
     exptimes = {"u_02": 38, "g_01": 30, "r_03": 30, "i_06": 30, "z_03": 30, "y_04": 30}
     # 1 --> single 30 second exposure
