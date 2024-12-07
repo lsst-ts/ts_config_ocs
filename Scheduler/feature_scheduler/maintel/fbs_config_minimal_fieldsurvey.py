3a10fef18656933504df611f2e69bf7a3fd2b73e
Keith Bechtol <kbechtol@lsst.org>
2024-12-07 22:06:10 +0000

    MTScheduler: filter swap to use grz for FBS minimal SV survey

--- Scheduler/feature_scheduler/maintel/fbs_config_minimal_fieldsurvey.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_minimal_fieldsurvey.py
@@ -45,8 +45,8 @@ def get_scheduler():
 
     # ComCam Deep Drilling Fields
 
-    nvisits = {"u_02": 5, "g_01": 5, "r_03": 5, "i_06": 5, "z_03": 5, "y_04": 5}
-    sequence = ["g_01", "r_03", "i_06"]
+    nvisits = {"u_02": 5, "g_01": 5, "r_03": 5, "i_06": 5, "z_03": 25, "y_04": 5}
+    sequence = ["g_01", "r_03", "z_03"]
     # exposure time in seconds
     exptimes = {"u_02": 38, "g_01": 30, "r_03": 30, "i_06": 30, "z_03": 30, "y_04": 30}
     # 1 --> single 30 second exposure
@@ -102,8 +102,8 @@ def get_scheduler():
 
     # Ecliptic Field
 
-    nvisits = {"u_02": 8, "g_01": 12, "r_03": 8, "i_06": 8, "z_03": 8, "y_04": 8}
-    sequence = ["g_01", "r_03", "i_06"]
+    nvisits = {"u_02": 8, "g_01": 8, "r_03": 8, "i_06": 8, "z_03": 20, "y_04": 8}
+    sequence = ["g_01", "r_03", "z_03"]
     # exposure time in seconds
     exptimes = {"u_02": 38, "g_01": 30, "r_03": 30, "i_06": 30, "z_03": 30, "y_04": 30}
     # 1 --> single 30 second exposure
