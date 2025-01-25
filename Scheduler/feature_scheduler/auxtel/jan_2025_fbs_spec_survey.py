4087839008aa4451914f13bb75646182471225a8
Lynne Jones <ljones.uw@gmail.com>
2025-01-24 23:29:20 -0800

    ATScheduler: Make configuration compatible with OPSIM-1217 updates in ts_fbs_utils

--- Scheduler/feature_scheduler/auxtel/jan_2025_fbs_spec_survey.py
+++ Scheduler/feature_scheduler/auxtel/jan_2025_fbs_spec_survey.py
@@ -21,7 +21,7 @@
 
 from astropy import units
 from astropy.coordinates import Angle
-from lsst.ts.fbs.utils import Target, Tiles
+from lsst.ts.fbs.utils import Target
 from lsst.ts.fbs.utils.auxtel.make_scheduler import MakeScheduler, SurveyType
 from rubin_scheduler.scheduler.detailers import DitherDetailer
 
@@ -37,6 +37,7 @@ def get_scheduler():
         Feature based scheduler.
     """
     nside = 64
+    # Some basic values to weight surveys up or down within their tier
     reward_values = dict(
         default=5.0,
         image_pole=1.0,
@@ -46,21 +47,21 @@ def get_scheduler():
         spec_boost=20.0,
     )
 
-    image_nexp = 1  # number of exposures
+    image_nexp = 5  # number of exposures in sequence (per filter)
     image_exptime = 35.0  # total exposure time in seconds
-    image_visit_gap = 12 * 60.0
+    image_visit_gap = 0  # gap between visits, in minutes
     wind_speed_maximum = 13.0  # maximum direct wind in m/s
     avoid_wind = False
-    cwfs_time_gap = 120.0
+    cwfs_time_gap = 120.0  # Gap between cwfs images, in minutes
 
     spec_ha_limit = [
         (0.0, 12.0),
         (14.2, 24.0),
     ]
-    image_ha_limit = [
-        (21.0, 24.0),
-        (0, 3.0),
-    ]
+    # image_ha_limit = [
+    #    (21.0, 24.0),
+    #    (0, 3.0),
+    # ]
 
     spec_target_list = [
         Target(
@@ -72,7 +73,8 @@ def get_scheduler():
             reward_value=reward_values["spec_boost"],
             filters=["r"],
             visit_gap=0.0,
-            exptime=420.0,
+            # bright spectroscopy block is shorter
+            exptime=300.0,
             nexp=1,
         ),
         Target(
@@ -89,21 +91,29 @@ def get_scheduler():
         ),
     ]
 
-    image_tiles = [
-        Tiles(
+    image_target_list = [
+        Target(
+            target_name="Photo0800-1",
             survey_name="BLOCK-306",
-            hour_angle_limit=image_ha_limit,
+            ra=Angle("08:10:00", unit=units.hourangle),
+            dec=Angle("-36:11:23", unit=units.deg),
+            hour_angle_limit=None,  # image_ha_limit,
             reward_value=reward_values["default"],
             filters=["r"],
             visit_gap=image_visit_gap,
             exptime=image_exptime,
             nexp=image_nexp,
-        ),
+        )
     ]
 
     spec_detailers = []
-    image_detailers = [
-        DitherDetailer(max_dither=(25.0 / (60.0 * 60.0)), per_night=True)
+    image_detailers_tile = [
+        # Tiny dither night-to-night. (does this work?)
+        DitherDetailer(max_dither=(25 / 60 / 60), per_night=True)
+    ]
+    image_detailers_target = [
+        # Dither up to half the FOV, per exposure.
+        DitherDetailer(max_dither=(7 / 2 / 60), per_night=False)
     ]
 
     make_scheduler = MakeScheduler()
@@ -111,14 +121,17 @@ def get_scheduler():
     return make_scheduler.get_scheduler(
         nside=nside,
         wind_speed_maximum=wind_speed_maximum,
-        survey_type=SurveyType.ImageSpec,
+        survey_type=SurveyType.SpecImage,
         spec_targets=spec_target_list,
-        image_tiles=image_tiles,
+        image_tiles=[],
+        image_targets=image_target_list,
         spec_detailers=spec_detailers,
-        image_detailers=image_detailers,
+        image_detailers_tiles=image_detailers_tile,
+        image_detailers_targets=image_detailers_target,
         cwfs_block_name="BLOCK-305",
         avoid_wind=avoid_wind,
         cwfs_time_gap=cwfs_time_gap,
+        equal_spec_image=False,
     )
 
 
