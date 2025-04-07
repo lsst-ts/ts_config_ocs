ae3cd7ae34c8abe762c631aaab1434045da87177
Keith Bechtol <kbechtol@lsst.org>
2025-04-07 05:02:42 +0000

    MTScheduler: update FBS config for on-sky field survey SV observations

--- Scheduler/feature_scheduler/maintel/fbs_config_minimal_fieldsurvey.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_minimal_fieldsurvey.py
@@ -21,6 +21,7 @@
 
 from pathlib import Path
 
+import numpy as np
 from lsst.ts.fbs.utils.maintel.make_fieldsurvey_scheduler import (
     MakeFieldSurveyScheduler,
     get_sv_targets,
@@ -66,13 +67,38 @@ def get_scheduler():
         targets=targets, nside=nside, ntiers=1, band_to_filter=band_to_filter
     )
 
-    nvisits = {"u": 5, "g": 5, "r": 5, "i": 5, "z": 5, "y": 5}
-    sequence = ["g", "r", "i"]
+    # Defaults
+
+    config_basis_functions = [
+        basis_functions.NotTwilightBasisFunction(sun_alt_limit=-12.0),
+        basis_functions.AltAzShadowMaskBasisFunction(
+            nside=nside,
+            min_alt=30.0,
+            max_alt=83.0,
+            shadow_minutes=10.0,
+        ),
+        basis_functions.AltAzShadowTimeLimitedBasisFunction(
+            nside=nside,
+            min_alt=30.0,
+            max_alt=83.0,
+            min_az=180.0,
+            max_az=270.0,
+            shadow_minutes=10.0,
+            time_to_sun=3.0,
+            sun_keys=["sunrise"],
+        ),
+        basis_functions.SlewtimeBasisFunction(bandname=None, nside=nside),
+    ]
+
+    observation_reason = "small field science"
+    science_program = "BLOCK-365"  # json BLOCK to be used
+
+    nvisits = {"u": 30, "g": 30, "r": 30, "i": 30, "z": 30, "y": 30}
+    sequence = ["u", "g", "r", "i", "z", "y"]
     # exposure time in seconds
-    exptimes = {"u": 38, "g": 30, "r": 30, "i": 30, "z": 30, "y": 30}
+    exptimes = {"u": 38.0, "g": 30.0, "r": 30.0, "i": 30.0, "z": 30.0, "y": 30.0}
     # 1 --> single 30 second exposure
     nexps = {"u": 1, "g": 1, "r": 1, "i": 1, "z": 1, "y": 1}
-
     field_survey_kwargs = {
         "nvisits": nvisits,
         "sequence": sequence,
@@ -80,34 +106,187 @@ def get_scheduler():
         "nexps": nexps,
     }
 
-    config_basis_functions = [
-        # TODO: return NotTwilightBasisFunction when rotator test is completed.
-        # basis_functions.NotTwilightBasisFunction(sun_alt_limit=-12.0),
-        basis_functions.AltAzShadowMaskBasisFunction(
-            nside=nside,
-            min_alt=30.0,
-            max_alt=83.0,
-            shadow_minutes=10.0,
-        ),
-        basis_functions.SlewtimeBasisFunction(bandname=None, nside=nside),
-        # TODO: add basis function to mask out azimuth range at end of night
-        # once SP-2080 functionality is available at summit.
-    ]
+    # LARGE FILL FACTOR TARGETS
 
+    field_survey_kwargs["sequence"] = ["u", "g", "r", "i"]
+
+    # Carina
+    # Custom landscape dither
+    radius = np.sqrt(1.8)
+    orientation = np.radians(-10.0)
+    delta_ra = [-1.0 * radius * np.cos(orientation), radius * np.cos(orientation)]
+    delta_dec = [-1.0 * radius * np.sin(orientation), radius * np.sin(orientation)]
     config_detailers = [
         detailers.DitherDetailer(max_dither=0.7, per_night=False),
+        detailers.DeltaCoordDitherDetailer(delta_ra=delta_ra, delta_dec=delta_dec),
+        # Note: 2 centers * 2 deg per visit * 30 visits = 120 deg
+        detailers.CameraSmallRotPerObservationListDetailer(
+            max_rot=60.0,
+            min_rot=-60.0,
+            per_visit_rot=2.0,
+        ),
+    ]
+    tier = 0
+    target_names = ["Carina"]
+    make_scheduler.add_field_surveys(
+        tier,
+        observation_reason,
+        science_program,
+        target_names,
+        basis_functions=config_basis_functions,
+        detailers=config_detailers,
+        **field_survey_kwargs,
+    )
+
+    # Trifid-Lagoon
+    # Custom landscape dither
+    radius = np.sqrt(1.8)
+    orientation = np.radians(-10.0)
+    delta_ra = [-1.0 * radius * np.cos(orientation), radius * np.cos(orientation)]
+    delta_dec = [-1.0 * radius * np.sin(orientation), radius * np.sin(orientation)]
+    config_detailers = [
+        detailers.DitherDetailer(max_dither=0.7, per_night=False),
+        detailers.DeltaCoordDitherDetailer(delta_ra=delta_ra, delta_dec=delta_dec),
+        # Note: 2 centers * 2 deg per visit * 30 visits = 120 deg
+        detailers.CameraSmallRotPerObservationListDetailer(
+            max_rot=60.0,
+            min_rot=-60.0,
+            per_visit_rot=2.0,
+        ),
+    ]
+    tier = 0
+    target_names = ["Trifid-Lagoon"]
+    make_scheduler.add_field_surveys(
+        tier,
+        observation_reason,
+        science_program,
+        target_names,
+        basis_functions=config_basis_functions,
+        detailers=config_detailers,
+        **field_survey_kwargs,
+    )
+
+    # Prawn
+    # Custom landscape dither
+    radius = np.sqrt(1.8)
+    orientation = np.radians(10.0)
+    delta_ra = [-1.0 * radius * np.cos(orientation), radius * np.cos(orientation)]
+    delta_dec = [-1.0 * radius * np.sin(orientation), radius * np.sin(orientation)]
+    config_detailers = [
+        detailers.DitherDetailer(max_dither=0.7, per_night=False),
+        detailers.DeltaCoordDitherDetailer(delta_ra=delta_ra, delta_dec=delta_dec),
+        # Note: 2 centers * 2 deg per visit * 30 visits = 120 deg
+        detailers.CameraSmallRotPerObservationListDetailer(
+            max_rot=60.0,
+            min_rot=-60.0,
+            per_visit_rot=2.0,
+        ),
+    ]
+    tier = 0
+    target_names = ["Prawn"]
+    make_scheduler.add_field_surveys(
+        tier,
+        observation_reason,
+        science_program,
+        target_names,
+        basis_functions=config_basis_functions,
+        detailers=config_detailers,
+        **field_survey_kwargs,
+    )
+
+    # LOW ECLIPTIC LATITUDE TARGETS
+
+    field_survey_kwargs["sequence"] = ["g", "r", "i"]
+
+    # Low ecliptic latitude, near opposition April / May
+    # Detector scale dithers
+    config_detailers = [
+        detailers.DitherDetailer(max_dither=0.2, per_night=False),
+        # Note: 1 center * 3 deg per visit * 30 visits = 90 deg
         detailers.CameraSmallRotPerObservationListDetailer(
             max_rot=45.0,
             min_rot=-45.0,
-            per_visit_rot=1.0,
+            per_visit_rot=3.0,
         ),
     ]
-
-    observation_reason = "rotator_tracking_test"
-    science_program = "BLOCK-T423"  # json BLOCK to be used
-
     tier = 0
-    target_names = ["rotator_test_target"]
+    target_names = [
+        "Rubin_SV_216_-17",
+        "Rubin_SV_225_-19",
+    ]
+    make_scheduler.add_field_surveys(
+        tier,
+        observation_reason,
+        science_program,
+        target_names,
+        basis_functions=config_basis_functions,
+        detailers=config_detailers,
+        **field_survey_kwargs,
+    )
+
+    # LSST DEEP DRILLING FIELDS
+
+    field_survey_kwargs["sequence"] = ["u", "g", "r", "i", "z", "y"]
+
+    # LSST DDFs
+    # Detector scale dithers
+    config_detailers = [
+        detailers.DitherDetailer(max_dither=0.2, per_night=False),
+        # Note: 1 center * 3 deg per visit * 30 visits = 90 deg
+        detailers.CameraSmallRotPerObservationListDetailer(
+            max_rot=45.0,
+            min_rot=-45.0,
+            per_visit_rot=3.0,
+        ),
+    ]
+    tier = 0
+    target_names = [
+        "COSMOS",
+        "ELAIS_S1",
+        "XMM_LSS",
+        "ECDFS",
+    ]
+    make_scheduler.add_field_surveys(
+        tier,
+        observation_reason,
+        science_program,
+        target_names,
+        basis_functions=config_basis_functions,
+        detailers=config_detailers,
+        **field_survey_kwargs,
+    )
+
+    # DENSELY DITHERED STAR FIELDS
+
+    field_survey_kwargs["sequence"] = ["u", "g", "r", "i", "z", "y"]
+
+    # Densely dithered star fields
+    # Spectrophotometric calibrators
+    # Default for other fields
+    # 2x raft scale dithers
+    config_detailers = [
+        detailers.DitherDetailer(max_dither=1.4, per_night=False),
+        # Note: 1 center * 3 deg per visit * 30 visits = 90 deg
+        detailers.CameraSmallRotPerObservationListDetailer(
+            max_rot=45.0,
+            min_rot=-45.0,
+            per_visit_rot=3.0,
+        ),
+    ]
+    tier = 0
+    exclude = [
+        "Carina",
+        "Trifid-Lagoon",
+        "Prawn",
+        "Rubin_SV_216_-17",
+        "Rubin_SV_225_-19",
+        "COSMOS",
+        "ELAIS_S1",
+        "XMM_LSS",
+        "ECDFS",
+    ]
+    target_names = get_sv_targets(target_file, exclude=exclude)
+
     make_scheduler.add_field_surveys(
         tier,
         observation_reason,
