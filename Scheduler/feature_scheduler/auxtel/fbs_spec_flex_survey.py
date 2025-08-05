9e412fcde8c340ce8fe59a27496f7c7f3b60e5da
edennihy <edennihy@lsst.org>
2025-08-05 14:16:48 -0700

    ATScheduler: update auxtel_targets and fbs_spec_flex_survey priority targets.

--- Scheduler/feature_scheduler/auxtel/fbs_spec_flex_survey.py
+++ Scheduler/feature_scheduler/auxtel/fbs_spec_flex_survey.py
@@ -86,7 +86,7 @@ def get_scheduler():
     imaging_backup_targets = []
 
     # Spectroscopy priority - high priority spectroscopy - tier 1
-    spectroscopy_priority_targets = ["HD115169", "HD205905"]
+    spectroscopy_priority_targets = ["HD132096", "HD200654"]
     # Standard spectroscopy - tier 2
     spectroscopy_standard_targets = ["HD185975", "HD2811"]
 
